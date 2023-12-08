7cfaf304ac4de560a2b5bc1cb00cbaac4f497939
heydabop <heydabop@gmail.com>
2023-12-08 00:27:06 -0600

    day 8 part 2

--- src/bin/day08/main.rs
+++ src/bin/day08/main.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
 use regex::Regex;
+use std::collections::HashMap;
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
@@ -30,5 +29,56 @@ fn main() {
             }
         }
     }
-    println!("{steps}");
+    println!("part1: {steps}");
+
+    let mut starts = vec![];
+    for key in map.keys() {
+        if key.ends_with('A') {
+            starts.push(key)
+        }
+    }
+    let steps: Vec<u64> = starts
+        .into_iter()
+        .map(|mut pos| {
+            let mut steps = 0;
+            'outer: loop {
+                for &instruction in instructions {
+                    let next = &map[pos];
+                    match instruction {
+                        b'L' => pos = &next.0,
+                        b'R' => pos = &next.1,
+                        _ => panic!("unknown instruction {instruction}"),
+                    }
+                    steps += 1;
+                    if pos.ends_with('Z') {
+                        break 'outer;
+                    }
+                }
+            }
+            steps
+        })
+        .collect();
+    let mut synced_steps = steps[0];
+    for &step in &steps[1..] {
+        synced_steps = lcm(synced_steps, step);
+    }
+
+    println!("part2: {synced_steps}");
+}
+
+fn gcd(mut x: u64, mut y: u64) -> u64 {
+    // Euclidean gcd algorithm
+    // assumes x > 0 && y > 0
+    while x != y {
+        if x > y {
+            x -= y;
+        } else {
+            y -= x;
+        }
+    }
+    x
+}
+
+fn lcm(x: u64, y: u64) -> u64 {
+    (x / gcd(x, y)) * y
 }
